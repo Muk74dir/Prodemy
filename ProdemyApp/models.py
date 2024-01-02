a8b355716b6891f7e6949f2e05715752fd729ab0
Mofajjol <srshamol158850@gmail.com>
2024-01-03 00:02:39 +0600

    Update

--- ProdemyApp/models.py
+++ ProdemyApp/models.py
@@ -1,3 +1,204 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        if not username:
+            raise ValueError('User must have an username')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    #TODO this should be rename to USER 
+    first_name      = models.CharField(max_length=50)
+    last_name       = models.CharField(max_length=50)
+    username        = models.CharField(max_length=50, unique=True)
+    email           = models.EmailField(max_length=100, unique=True)
+    phone_number    = models.CharField(max_length=50)
+
+    # required
+    date_joined     = models.DateTimeField(auto_now_add=True)
+    last_login      = models.DateTimeField(auto_now=True)
+    is_admin        = models.BooleanField(default=False)
+    is_staff        = models.BooleanField(default=False)
+    is_active        = models.BooleanField(default=False)
+    is_superadmin        = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = MyAccountManager()
+        
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    def get_email(self):
+        return self.email
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
+
+
+
+# User Profile    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address_line = models.CharField(blank=True, max_length=100)
+    profile_picture = models.ImageField(blank=True, upload_to='photos/userprofile')
+    city = models.CharField(blank=True, max_length=20)
+    country = models.CharField(blank=True, max_length=20)
+
+    def __str__(self):
+        return self.user.first_name
+
+# Instructor Model
+class Instructor(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+
+
+
+# Learner Model
+class Learner(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+
+# Course Model
+class Course(models.Model):
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date=models.DateTimeField()
+    end_date=models.DateTimeField()
+
+# Feedback Model
+class Feedback(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+
+# Progress Tracker Model
+class ProgressTracker(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    progress_percentage = models.IntegerField()
+
+# Live Session Model
+class LiveSession(models.Model):
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE)
+    learner = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+
+# Discussion Forum Model
+class DiscussionForum(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=255)
+    content = models.TextField()
+    
+
+# Instructor Announcement Model
+class InstructorAnnouncement(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    message = models.TextField()
+
+# Exam/Assignment Model
+class Exam(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+
+
+# MCQ Evaluation Model
+class MCQEvaluation(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+# Badge Model
+class Badge(models.Model):
+    name = models.CharField(max_length=50)
+
+# Points Model
+class Points(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    points = models.IntegerField()
+
+# Blog Model
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+# Certificate Model
+class Certificate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_earned = models.DateField()
+
+# Payment Model
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=6,decimal_places=2)
+    date_paid = models.DateTimeField()
+
+# Coupon Model
+class Coupon(models.Model):
+    code = models.CharField(max_length=50)
+    discount_percentage = models.IntegerField()
+
+# Lecture Notes Model
+class LectureNotes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    content = models.TextField()
+
+# Data Security Model
+class DataSecurity(models.Model):
+    measure = models.CharField(max_length=255)
+
+# Video Download Model
+class VideoDownload(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    download_link = models.URLField()
+
+# Discount Model
+class Discount(models.Model):
+    coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
