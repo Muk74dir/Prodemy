5dd0ab7b0cd0463faf6947585d8c39f95529c5e5
Mofajjol <srshamol158850@gmail.com>
2024-01-03 23:56:56 +0600

    update

--- ProdemyApp/views.py
+++ ProdemyApp/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+# Updated Code
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import RegistrationForm,UserForm,UserProfileForm
+from .models import Account,UserProfile
+from orders.models import Order, OrderProduct
+from django.contrib import messages,auth
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-# Create your views here.
+from cart.views import _cart_id
+from cart.models import Cart,CartItem
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def register(request):
+    if request.method=='POST':
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name=form.cleaned_data['first_name']
+            last_name=form.cleaned_data['last_name']
+            phone_number=form.cleaned_data['phone_number']
+            email=form.cleaned_data['email']
+            password=form.cleaned_data['password']
+            username=email.split("@")[0]
+            user=Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number=phone_number
+            user.save()
+            
+    else:
+        form=RegistrationForm()
+    context={
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+    
+    
+def login(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        user=auth.authenticate(email=email, password=password)
+        
+        if user is not None:
+            try:
+                cart=Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists= CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item=CartItem.objects.filter(cart=cart)
+                    for item in cart_item:
+                        item.user=user
+                        item.save()
+            except:
+                pass
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You are logged out')
+    return redirect('login')
