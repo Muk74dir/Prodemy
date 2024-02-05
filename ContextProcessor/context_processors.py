d1e2707fdc49896fef17585f348452a360cc13b4
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-02-05 14:53:14 +0600

    update homepage

--- ContextProcessor/context_processors.py
+++ ContextProcessor/context_processors.py
@@ -1,4 +1,14 @@
-from ProdemyApp.models import CourseCategoryModel
+from ProdemyApp.models import CourseCategoryModel, CourseModel, User
 def Categories(request):
     categories = CourseCategoryModel.objects.all()
     return {'Categories': categories}
+
+def Course(request):
+    courses = CourseModel.objects.all()
+    for course in courses:
+        print(course.title)
+    return {'courses':courses}
+
+def Users(request):
+    users = User.objects.all()
+    return {'users':users}
